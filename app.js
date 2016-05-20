bda90086956f0ad6a7b2edf4b6156f69dec091b3
stanleynguyen <hung.ngn.the@gmail.com>
2016-05-20 02:41:15 +0000

    fix url major bug

--- app.js
+++ app.js
@@ -9,30 +9,24 @@ app.get('/', function(req, res){
 });
 
 app.get('/new/:url', function(req, res){
+    var url = req.params.url.replace(/itsaslash/g, '/').replace(/itsaquestion/g, '?').replace(/itsaequal/g, '=');
     mongo.connect('mongodb://stanley:hung123@ds025232.mlab.com:25232/lilurl', function(err, db){
         if(err) throw err;
         db.collection('lilurls').find({
-            'original': req.params.url
+            'original': url
         }).toArray(function(err, docs){
             if(err) throw err;
             if(docs.length === 0){
-                myEngine.getCount(myEngine.writeData, req.params.url, res, req);
+                myEngine.getCount(myEngine.writeData, url, res, req);
             }else{
                 var hostUrl = req.protocol+'://'+req['hostname']+'/';
-                res.render(__dirname+'/views/new.ejs', {oriUrl: req.params.url, shortUrl: hostUrl+docs[0]['_id']})
+                res.render(__dirname+'/views/new.ejs', {oriUrl: url, shortUrl: hostUrl+docs[0]['_id']});
             }
             db.close();
         });
     });
 });
 
-app.get('/new/http://:url', function(req, res){
-    res.redirect(req.protocol+'://'+req.hostname+'/new/'+req.params.url);
-})
-
-app.get('/new/https://:url', function(req, res){
-    res.redirect(req.protocol+'://'+req.hostname+'/new/'+req.params.url);
-})
 
 app.get('/:id', function(req, res){
     if(isNaN(Number(req.params.id))){
